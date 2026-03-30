77196ecd326842acc32a431de34cba99ee0126a6
Gareth8 <george.acklam.ga@gmail.com>
2026-03-30 13:16:24 +0100

    Slices.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let options = [
         "Control Flow",
         "Ownership",
-        "References and Borrowing"
+        "References and Borrowing",
+        "Slices"
     ];
     let mut count = 1;
 
@@ -41,6 +42,7 @@ fn main() {
             1 => control_flow(),
             2 => ownership(),
             3 => references_and_borrowing(),
+            4 => slices(),
             _ => println!("Please input a number between 1 and {options_size} inclusive.")
         }
     };
@@ -138,4 +140,43 @@ fn no_dangle() -> String {
     string
 
     // Return the value of string like this rather than as a reference prevents a dangling pointer.
+}
+fn slices() {
+    // Don't do it like this.
+    let mut s1 = String::from("hello world");
+    let word1 = first_words(&s1);
+    s1.clear();
+    println!("The first word is {word1}");
+
+    // Do it like this.
+    let s2 = String::from("hello world");
+    let word2 = better_first_words(&s2);
+    // s2.clear() won't work here, since it would be a mutable borrow inbetween two immutable borrows on the lines above and below.
+    println!("The first word is {word2}");
+    /*
+    The .. syntax is for a range, and the number before and after the .. relates to the start and end index of the slice.
+    If you are including index 0 of the string, you could go &s[..5]. Same with the last character, &s[0..].
+    A whole slice would just be &s[..]
+     */
+}
+fn first_words(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+} // Example of a bad way to do this.
+fn better_first_words(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
